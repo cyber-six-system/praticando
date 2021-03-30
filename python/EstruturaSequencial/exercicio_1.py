8c868a3d37a4ad685b5bbcd40888f07887bc8f8c
Julio Cesar Ovidio Bueno <info.juliocesar.ovidiobueno@gmail.com>
2021-03-29 22:33:15 -0300

    Upload dos arquivos atualizados (comentários) 2021.03.29

--- python/EstruturaSequencial/exercicio_1.py
+++ python/EstruturaSequencial/exercicio_1.py
@@ -1,2 +1,19 @@
+# -*- coding: utf-8 -*-
+
+""" 
+    Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+    Versão do Python em que foi testada: 3.9.2    
+"""
+
+__author__ = "Julio Bueno"
+__copyright__ = "Copyright 2021, by cyber-six-system"
+__credits__ = "Todos desenvolvedores de software livre"
+__license__ = "GNU General Public License"
+__version__ = "1.0.0"
+__maintainer__ = "cyber-six-system"
+__status__ = "Prototype"
+
+global MSG
 msg = "Hello World"
-print(msg)
+print(MSG)
--- python/EstruturaSequencial/exercicio_3.py
+++ python/EstruturaSequencial/exercicio_3.py
@@ -1,5 +1,36 @@
+# -*- coding: utf-8 -*-
+
+""" 
+    Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+    Versão do Python em que foi testada: 3.9.2    
+"""
+
+__author__ = "Julio Bueno"
+__copyright__ = "Copyright 2021, by cyber-six-system"
+__credits__ = "Todos desenvolvedores de software livre"
+__license__ = "GNU General Public License"
+__version__ = "1.0.0"
+__maintainer__ = "cyber-six-system"
+__status__ = "Prototype"
 
 def isNum(x):
+    """
+        Titulo:
+            É número? 
+
+        Descrição:
+            Verificar se dado que entrou é número
+
+            Este método também é responsável por validar que tipo de informação o usuário inseriu, 
+            se de fato é númerico.
+
+        Parâmetros:
+            :param x: int 
+                Variável inteira para tratamento da validação
+            :return Str
+        """
+
     while not x.isnumeric():
             x = input("Digite um número: ")
     return x
